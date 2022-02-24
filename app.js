e2272b177c82db7e06e4accde2c07d1bd4cca1b3
rileyscheer <68623637+rileyscheer@users.noreply.github.com>
2022-02-24 14:20:11 -0600

    Added full functionality

--- app.js
+++ app.js
@@ -10,30 +10,34 @@ const numbers = document.querySelectorAll('.num');
 const clear = document.querySelector('#clear');
 const del = document.querySelector('#delete');
 const equals = document.querySelector('#equals');
-const addBtn = document.querySelector('#add');
-const subtractBtn = document.querySelector('#subtract');
 const decimal = document.querySelector('#decimal');
-const multiplyBtn = document.querySelector('#multiply');
-const divideBtn = document.querySelector('#divide');
 
 
 let add = (x, y) => {
-    return x + y;
+    answer = x + y;
+    currentOperator = null;
+    return answer;
 }
 
 
 let subtract = (x, y) => {
-    return x - y;
+    answer = x - y;
+    currentOperator = null;
+    return answer;
 } 
 
 
 let multiply = (x, y) => {
-    return x * y;
+    answer =  x * y;
+    currentOperator = null;
+    return answer;
 }
 
 
 let divide = (x, y) => {
-    return x / y;
+    answer = x / y;
+    currentOperator = null;
+    return answer
 } 
 
 
@@ -47,7 +51,7 @@ function populateDisplay(number){
 
 function setOperation(operator) {
     if (currentOperator !== null) {
-        evaluate()
+        run()
     }
     firstOperator = result.textContent;
     currentOperator = operator;
@@ -60,7 +64,7 @@ function resetScreen() {
     reset = false;
   }
 
-function evaluate() {
+function run() {
     if (currentOperator === null || reset) {
         return;
     }
@@ -70,10 +74,10 @@ function evaluate() {
     }
     secondOperator = result.textContent;
     last.textContent = `${firstOperator} ${currentOperator} ${secondOperator} = `
-    result.textContent = roundResult(outcome(firstOperator, secondOperator));
+    result.textContent = round(outcome(firstOperator, secondOperator));
 }
 
-function roundResult (number) {
+function round(number) {
     return Math.round(number * 1000) / 1000
 }
 
@@ -107,7 +111,16 @@ numbers.forEach((button) =>
 operator.forEach((button) =>
   button.addEventListener('click', () => setOperation(button.textContent)))
 
-equals.addEventListener('click', evaluate)
+equals.addEventListener('click', run)
 clear.addEventListener('click', clearAll)
 
+decimal.addEventListener('click', () => {
+    result.textContent += '.';
+    if (result.textContent.includes('.')) {
+        return;
+    }
+})
 
+del.addEventListener('click', () => {
+    result.textContent = result.textContent.toString().slice(0, -1);
+})
